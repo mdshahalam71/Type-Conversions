8540f6f2f11ac3dbd884a83d453a55b3bab1ce66
Shah Alam <mdalam127671@gmail.com>
2025-07-29 22:00:33 +0600

    2 commite

---
+++ 2.8-OperatorsMaths.js
@@ -0,0 +1,23 @@
+ let a=('1'+2+3);   //
+ console.log(a);    //123
+
+ let b = (2+2+'1');
+ console.log(b);     //41
+
+ let c = (1+'2'+3);
+ console.log(c);    //123
+
+ let d = (6-'2');
+ console.log(d);    //4
+
+ let e = ('6'/'2');
+ console.log(e);    //3
+
+ let f = 1;
+ console.log(+f);   //1;
+
+ let g=-2;
+ console.log(+g);   //-2
+
+ console.log(+true);   //1
+ console.log(+"");     //0
