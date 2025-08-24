c5aab1234118d6fec77f0d6b1bf3e986de6518ed
Shah Alam <mdalam127671@gmail.com>
2025-08-24 18:32:13 +0600

    15 number solve this

--- problem-solve.js
+++ problem-solve.js
@@ -1,9 +1,18 @@
 //  Basic
-// 14.Write a JavaScript exercise to get the filename extension.
+// ....................14.Write a JavaScript exercise to get the filename extension.
 
-let filename="abcd.php";
- let fileExtension=filename.split('.').pop();
- console.log(fileExtension);
+// let filename="abcd.php";
+//  let fileExtension=filename.split('.').pop();
+//  console.log(fileExtension);
 
-  const filename2='abc.js'
-  console.log(filename2.split('.').pop());
+//   const filename2='abc.js'
+//   console.log(filename2.split('.').pop());
+
+// ....................15.Write a JavaScript program to get the difference between a given number and 15, if the number is broader than 15 return double the absolute difference.
+
+//  function difference(n){
+//     if(n<=15) return 15-n;
+//     else
+//         return (n-15)*3;
+//  }
+//  console.log(difference(null))
